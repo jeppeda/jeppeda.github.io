74b18a29a51d2da6e2757db09884587e70f6a7b3
jeppeda <jeppeda@gmail.com>
2018-11-08 14:32:10 +0100

    hyphae

--- hyphae/hyphae.js
+++ hyphae/hyphae.js
@@ -42,7 +42,7 @@ class Terrain {
         for(let i=0;i<this.size;i++) {
             let row = [];
             for(let j=0;j<this.size;j++) {
-                row.push(Math.random());
+                row.push(j===0||j===this.size||i===0||i===this.size ? 2 : Math.random());
             }
             this.values.push(row);
         }
@@ -58,19 +58,16 @@ class Terrain {
             [x,y+EC],[x,y-EC]
         ];
 
-        console.log('possiblePositions.length', possiblePositions.length);
-        possiblePositions = possiblePositions.filter(
-            possiblePosition => {
-                return path.findIndex(i=>JSON.stringify(i)===JSON.stringify(possiblePosition))===-1
-            }
+        possiblePositions = possiblePositions.filter(possiblePosition => 
+            path.findIndex(i=>JSON.stringify(i)===JSON.stringify(possiblePosition))===-1 || possiblePosition[0]===0 || possiblePosition[1]===0
             );
-        console.log('possiblePositions.length filtered', possiblePositions.length);
-
+        if(possiblePositions.length === 0) {
+            console.log('hov');
+        }
         let possiblePositionValues = possiblePositions.map(p=>this.get(p[0],p[1]));
         let minval = Math.min(...possiblePositionValues);
         let minpos = possiblePositions[possiblePositionValues.findIndex(i=>i===minval)];
 
-        console.log('minpos', minpos);
         return minpos;
     }
 }
